6afd793b43ecaf521f0af97a5d25436f49b22064
sneh18 <dalalsneh18@gmail.com>
2022-08-31 17:44:06 +0530

    [Automation Project] Added support to add logs archive details to inventory.html and set cron job

--- automation.sh
+++ automation.sh
@@ -109,8 +109,38 @@ if [ $? -eq 0 ];
 then
         echo " : Done"
 else
-        echo " : Failed (Exiting the script)"
-        exit
+        echo " : Failed"
+fi
+
+# Check if inventory.html file is present or not and create one if it is not present
+if [ -e /var/www/html/inventory.html ]
+then
+        echo "inventory.html file is already present at /var/www/html location"
+else
+        echo -n "Creating inventory.html file at /var/www/html location with default headers"
+        touch /var/www/html/inventory.html
+        printf "Log Type\tDate Created\tType\tSize\n" >> /var/www/html/inventory.html
+        echo " : Done"
+fi
+
+# Getting logs archive file details like timestamp, type and size and appending those to inventory.html file
+log_type="httpd-logs"
+file_timestamp=$(echo "$archive_file_name" | awk -F'[-.]' '{print $4"-"$5}')
+file_type=$(echo "$archive_file_name" | awk -F'[.]' '{print $2}')
+file_size=$(du -s -h /tmp/$archive_file_name 2>/dev/null | awk '{ print $1 }')
+echo -n "Appending latest entry of exported logs archive to inventory.html file"
+printf '%s\t%s\t%s\t%s\n' "$log_type" "$file_timestamp" "$file_type" "$file_size" >> /var/www/html/inventory.html
+echo " : Done"
+
+# Check if automation cron job is already set or not and create one if it is not present
+if [ -e /etc/cron.d/automation ]
+then
+        echo "Cron Job file is already present"
+else
+        echo -n "Creating a Cron Job file to run this script everyday at 12:00 AM"
+        touch /etc/cron.d/automation
+        echo "0 0 * * * root /root/Automation_Project/automation.sh" > /etc/cron.d/automation
+        echo " : Done"
 fi
 
 # Exiting the script with successful execution code
